0013f6978765b16453e21c571ff94d3fecef5310
Talha Muhammad <121675123+talha1230@users.noreply.github.com>
2024-08-12 15:00:49 +0800

    Update footer link to mention the purpose of the dashboard

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,7 +1,7 @@
  <div class="row">
                         <div class="col-lg-12">
                             <div class="footer">
-                                <p>This dashboard was generated on <a href="https://github.com/talha1230">UNIMY</a></p>
+                                <p>This dashboard was generated for <a href="https://github.com/talha1230">UNIMY</a></p>
                             </div>
                         </div>
                     </div>
